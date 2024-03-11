6b61f961c5ef0e0c2c3da9dc4149802751ba0029
Abdalwanees <gabdo7682@gmail.com>
2024-03-11 05:29:05 +0200

    Change Payment Url

--- src/app/shared/srvices/checkout.service.ts
+++ src/app/shared/srvices/checkout.service.ts
@@ -3,48 +3,46 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CheckoutService {
+  constructor(private _HttpClient: HttpClient) {}
 
-  constructor(private _HttpClient:HttpClient) { }
+  header: any = { token: localStorage.getItem('userToken') };
 
-
-
-  header:any={token: localStorage.getItem('userToken')}
-
-  checkout(id:string , data:object):Observable<any>{
-    return this._HttpClient.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${id}?url=http://localhost:4200`,
-    {
-      shippingAddress:data
-    },
+  checkout(id: string, data: object): Observable<any> {
+    return this._HttpClient.post(
+      `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${id}?url=https://abdalwanees.github.io/Fresh-Cart/`,
       {
-        headers:this.header
-      }
-
-    )
-    }
-
-    cashout(id:string , data:object):Observable<any>{
-      return this._HttpClient.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${id}`,
-      {
-        shippingAddress:data
+        shippingAddress: data,
       },
-        {
-          headers:this.header
-        }
-  
-      )
+      {
+        headers: this.header,
       }
+    );
+  }
 
+  cashout(id: string, data: object): Observable<any> {
+    return this._HttpClient.post(
+      `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${id}`,
+      {
+        shippingAddress: data,
+      },
+      {
+        headers: this.header,
+      }
+    );
+  }
 
-    getAllOrders():Observable<any>{
-      return this._HttpClient.get('https://ecommerce.routemisr.com/api/v1/orders')
-    }
+  getAllOrders(): Observable<any> {
+    return this._HttpClient.get(
+      'https://ecommerce.routemisr.com/api/v1/orders'
+    );
+  }
 
-    getUserOrders(id:string):Observable<any>{
-      return this._HttpClient.get(`https://ecommerce.routemisr.com/api/v1/orders/user/${id}`)
-    }
-
-
+  getUserOrders(id: string): Observable<any> {
+    return this._HttpClient.get(
+      `https://ecommerce.routemisr.com/api/v1/orders/user/${id}`
+    );
+  }
 }
